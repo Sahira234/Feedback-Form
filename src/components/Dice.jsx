7c4005fef4f796c325965d3b6469b8a86e9af5a2
Shovgu <mmmdov208@gmail.com>
2022-11-21 18:07:53 +0400

    zere kliklendikde random number

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -6,13 +6,16 @@ const [state,setState]=useState(1)
 useEffect(()=>{
     window.addEventListener("keypress",(e)=>{
         if(e.keyCode==13){
-setState(Math.floor(Math.random() * 6) + 1)
+setState(Math.floor(Math.random() * 6) + 1);
         }
     })
 })
+const handleClick=()=>{
+    setState(Math.floor(Math.random() * 6) + 1);
+}
   return (
     <div>
-        <div className={styles.kare}>
+        <div className={styles.kare} onClick={handleClick}>
 {state}
         </div>
     </div>
